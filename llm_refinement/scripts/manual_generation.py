a304030472f205a2d82bddd0e7fb01de708f6289
alkhoun99 <nouralkhoury01@gmail.com>
2023-12-23 13:22:26 +0200

    Added reading clinical trials ids from json file

--- llm_refinement/scripts/manual_generation.py
+++ llm_refinement/scripts/manual_generation.py
@@ -3,14 +3,19 @@ import argparse
 from modules.chromadb_handler import ChromaDBHandler
 
 
-def get_trials_data(collection):
-    ids = [
-            "NCT05061550", "NCT05062278", "NCT05065398", "NCT05053802",
-            "NCT04293796", "NCT04222335", "NCT04222972", "NCT04241731",
-            "NCT04670679", "NCT04461808", "NCT04536077", "NCT03817268",
-            "NCT03475121", "NCT03390686", "NCT05354388", "NCT05430802",
-            "NCT05241873", "NCT05639751", "NCT04771520"
-            ]
+def get_trials_data(collection, ids):
+    """
+    Retrieve data for a list of clinical trial IDs from the ChromaDB
+    collection.
+
+    Args:
+        collection (ChromaDB collection): The ChromaDB collection.
+        ids (list): List of clinical trial IDs.
+
+    Returns:
+        dict: Dictionary containing the size of the dataset and the extracted
+        data.
+    """
 
     results = {
         "size": len(ids),
@@ -28,7 +33,8 @@ def get_trials_data(collection):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Generate manual clinical trials.")
+    parser = argparse.ArgumentParser(description="""Generate manual clinical
+                                     trials.""")
     parser.add_argument("--persist-dir",
                         required=True,
                         help="Path to the ChromaDB persist directory"
@@ -37,6 +43,12 @@ def main():
     parser.add_argument("--collection",
                         required=True,
                         help="ChromaDB collection name")
+
+    parser.add_argument("--input-file",
+                        required=True,
+                        help="""Path to the JSON file containing the ids for
+                        the manually selected trials."""
+                        )
 
     parser.add_argument("--output-dir",
                         required=True,
@@ -48,7 +60,10 @@ def main():
         persist_dir=args.persist_dir,
         collection_name=args.collection).collection
 
-    results = get_trials_data(collection=collection)
+    with open(args.input_file, "r") as f:
+        ids = json.load(f)['ids']
+
+    results = get_trials_data(collection=collection, ids=ids)
 
     with open(f"{args.output_dir}/manual_test_set.json", "w") as f:
         json.dump(results, f, indent=4)
