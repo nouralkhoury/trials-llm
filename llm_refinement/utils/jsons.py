f08b88663676ac0a88678bff19a7f8dd1d95aa86
alkhoun99 <nouralkhoury01@gmail.com>
2024-02-18 21:27:47 +0100

    remove try/except from jsons load func

--- llm_refinement/utils/jsons.py
+++ llm_refinement/utils/jsons.py
@@ -2,14 +2,9 @@ import json
 
 
 def load_json(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-        return data
-    except FileNotFoundError:
-        print(f"File not found: {file_path}")
-    except json.JSONDecodeError as e:
-        print(f"Error decoding JSON in file {file_path}: {e}")
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
 
 
 def dump_json(data, file_path):
