98921d9a0113208fb0486040197fefb4a5707777
alkhoun99 <nouralkhoury01@gmail.com>
2024-02-18 21:32:57 +0100

    Modified GPTHandler to accept a PromptTemplate
    Used to load pre-saved PromptTemplates objects in JSON
    
    For easier reproducibility

--- llm_refinement/modules/gpt_handler.py
+++ llm_refinement/modules/gpt_handler.py
@@ -1,6 +1,5 @@
 import sys
 import promptlayer
-from langchain.prompts.prompt import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.chat_models import PromptLayerChatOpenAI
@@ -24,7 +23,6 @@ class GPTHandler(object):
         if pl_tags:
             promptlayer.api_key = PROMPTLAYER_API_KEY
             llm = PromptLayerChatOpenAI(pl_tags=["zero-shot-gpt-4"],
-                                        openai_api_key=OPENAI_API_KEY,
                                         temperature=0,
                                         model=model_name)
         else:
@@ -32,39 +30,6 @@ class GPTHandler(object):
                              model=model_name,
                              temperature=0)
         return llm
-
-    @staticmethod
-    def prompt_setup(template: str, num_examples: int = 0) -> dict:
-        """
-        Sets up a prompt template.
-
-        Args:
-        template: The template prompt to be used
-        num_examples: The number of examples to be used in few-shot. Default: 0 (zero-shot)
-
-        Returns:
-        dict: A dictionary containing the prompt template.
-        """
-        try:
-            if not isinstance(num_examples, int):    
-                raise TypeError("Invalid 'num_examples'. Expected an integer.")
-
-            if not isinstance(template, str):
-                raise TypeError("Invalid 'template'. Expected a string.")
-        except TypeError as e:
-            print(f"Prompt setup failed: {e}")
-
-        if num_examples == 0:
-            prompt_template = PromptTemplate(
-                input_variables=["trial"],
-                template=template)
-        else:
-            # setup the example variable
-            examples = [f"example_{i}" if num_examples > 1 else "example" for i in range(1, num_examples + 1)]
-            prompt_template = PromptTemplate(
-                input_variables=["trial"] + examples,
-                template=template)
-        return prompt_template
 
     @staticmethod
     def chain_setup(llm, prompt_template):
@@ -80,21 +45,13 @@ class GPTHandler(object):
         chain = LLMChain(llm=llm, prompt=prompt_template)
         return chain
 
-    def setup_gpt(self, model_name: str = "gpt-3.5-turbo", template=str, num_examples: int = 0, pl_tags: list = []):
+    def setup_gpt(self, prompt_template, model_name: str = "gpt-3.5-turbo", pl_tags: list = []):
         try:
             # llm setup
             llm = self.llm_setup(model_name, pl_tags)
         except Exception as e:
             print(f"Error setting up LLM {e}", file=sys.stderr)
             return
-
-        try:
-            # prompt setup
-            prompt_template = self.prompt_setup(template, num_examples)
-        except Exception as e:
-            print(f"Error setting up prompt template {e}", file=sys.stderr)
-            return
-
         try:
             # chain setup
             chain = self.chain_setup(llm, prompt_template)
