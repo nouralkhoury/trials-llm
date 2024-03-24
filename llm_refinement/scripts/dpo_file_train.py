a3506cd4b9a89fee82139530299309bfd8228dd2
alkhoun99 <nouralkhoury01@gmail.com>
2024-03-24 16:22:07 +0100

    dpo

---
+++ llm_refinement/scripts/dpo_file_train.py
@@ -0,0 +1,108 @@
+import gc
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, BitsAndBytesConfig
+from peft import LoraConfig, PeftModel
+from trl import DPOTrainer
+import wandb
+from datasets import Dataset
+
+model_name = "NousResearch/Hermes-2-Pro-Mistral-7B"
+new_model = "Hermes-2-Pro-Mistral-7B-dpo"
+import json
+
+# Specify the path to the JSON file
+file_path = "/data/local/mshaik/onconaut/notebooks/negative.jsonl"
+
+#read jsonl file
+system_prompt = f"""<|im_start|>system\nYou are a helpful assistant that extracts only genomic biomarkers from the supplied clinical trial data and responds in JSON format. Here's the json schema you must adhere to:<schema>{{\"inclusion_biomarker\": [[]], \"exclusion_biomarker\": [[]]}}</schema>\nIn this context, limit the extraction of genomic biomarkers to the following categories: gene alteration (mutation, fusion, rearrangement, copy number alteration, deletion, insertion, translocation), pathway alterations, gene expression, protein expression, pathway expression, HLA, TMB (tumor molecular burden, TMB-H or TMB-L), MSI (microsatellite instability, MSI-H, MSI-L, MSS, microsatellite stable) status, gene pathway alteration like dMMR (deficient Mismatch Repair Pathway) or pMMR (proficient Mismatch Repair), and protein status (HER2, ER, PgR, PD-L1).\n\nDo not extract non-genomic biomarkers, which refer to any indicators not directly related to genetic or genomic information. Ignore information such as age, medical conditions, potential pregnancy, disease stage, allergies, treatment history, drugs, therapies, treatment, histology, and tumor cancer types, diseases, HIV, infections, and more. Also, ignore information about levels, scores, doses, expression ratios, and illnesses. Do not consider biomarkers related to model experimental animals, historical data, or previous studies.\n\nPreserve logical connections (AND, OR) between genomic biomarkers. Group 'AND'-linked genomic biomarkers in the same list, and place 'OR'-linked genomic biomarkers in separate lists. Treat main bullets in \"Inclusion Criteria\" as AND logic, and \"Exclusion Criteria\" as OR logic, unless specified otherwise. Handle ambiguous logic in the sentence as OR.\n\nEnsure each genomic biomarker is a string with the gene name preceding the variant. Remove the words \"gene\", \"allele\", \"status\", and \"mutation\" (when a specific variant is given). Make the variant singular and noun-based. Replace \"mutant\" with \"mutation\". Include a space between the gene name, its variant if they are connected. Include a space between the hormone name and its status if they are connected. Replace \"positive expression\" with \"expression\" and symbols \"-\" and \"+\" with \"negative\" and \"positive\" respectively, except in MSI status or known fusions separated by \"-\". Add \"germline\" or \"somatic\" terms in parentheses at the end of the corresponding biomarker. Ignore biomarkers mentioned as \"exceptions\" or after \"other than\". Handle synonyms in parentheses by extracting the genomic biomarker but ignoring the synonym. Extract each genomic biomarker once. Expand the genomic biomarkers when needed.\n\nTo summarize, extract only genomic biomarkers from the supplied clinical trial data, focusing on the categories mentioned above. Ignore any non-genomic biomarkers and unrelated information such as age, medical conditions, treatment history, cancer, drugs, therapies, histology, levels and scores. If no genomic biomarkers are found, return empty lists in JSON. Do not make assumptions or add biomarkers. Do not add any biomarkers that are not explicitly mentioned in the input, and do not make assumptions about potential genomic biomarkers. Ensure output list contains only lists of strings when there exist genomic biomarkers in the input, following this example: {{\"inclusion_biomarker\": [[\"GeneA variantA\"], [\"GeneX variantY]], \"exclusion_biomarker\": []}}. Do not \\escape. Do not repeat a genomic biomarker.<|im_end|>\n"""
+user = f"""<|im_start|>user\nExtract the genomic biomarker from the clinical trial below. Just generate the JSON object without explanation."""
+user_end = f"""\n<|im_end|>\n<|im_start|>assistant"""
+with open(file_path, 'r') as file:
+    data = [json.loads(line) for line in file]
+
+
+
+data_set_new = {"prompt": [], "chosen": [], "rejected": []}
+for da in data:
+    data_set_new["prompt"].append(system_prompt+user+da["input"]+user_end)
+    data_set_new["chosen"].append(str(da["output"]))
+    data_set_new["rejected"].append(da["rejected"])
+
+
+# Convert data to Hugging Face dataset
+dataset = Dataset.from_dict(data_set_new)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "left"
+# LoRA configuration
+peft_config = LoraConfig(
+    r=16,
+    lora_alpha=16,
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules=['k_proj', 'gate_proj', 'v_proj', 'up_proj', 'q_proj', 'o_proj', 'down_proj']
+)
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+model = AutoModelForCausalLM.from_pretrained(model_name, quantization_config=bnb_config, device_map="auto")
+
+# Training arguments
+training_args = TrainingArguments(
+    per_device_train_batch_size=1,
+    gradient_accumulation_steps=4,
+    gradient_checkpointing=True,
+    learning_rate=5e-5,
+    lr_scheduler_type="cosine",
+    max_steps=200,
+    save_strategy="no",
+    logging_steps=1,
+    output_dir=new_model,
+    optim="paged_adamw_32bit",
+    warmup_steps=100,
+    bf16=False,
+    report_to="wandb",
+)
+
+# Create DPO trainer
+dpo_trainer = DPOTrainer(
+    model,
+    args=training_args,
+    train_dataset=dataset,
+    tokenizer=tokenizer,
+    peft_config=peft_config,
+    beta=0.1,
+    max_length=4800,
+)
+
+# Fine-tune model with DPO
+dpo_trainer.train()
+# Save artifacts
+dpo_trainer.model.save_pretrained("final_checkpoint")
+tokenizer.save_pretrained("final_checkpoint")
+
+# Flush memory
+del dpo_trainer, model
+gc.collect()
+torch.cuda.empty_cache()
+
+# Reload model in FP16 (instead of NF4)
+base_model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    return_dict=True,
+    torch_dtype=torch.float16,
+)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# Merge base model with the adapter
+model = PeftModel.from_pretrained(base_model, "final_checkpoint")
+model = model.merge_and_unload()
+
+# Save model and tokenizer
+model.save_pretrained(new_model)
+tokenizer.save_pretrained(new_model)
